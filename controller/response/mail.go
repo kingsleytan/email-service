5ba2640b4afbbb92fcfb5bbcf6955eee15f8f54d
kingsleytan <sengking.tan@gmail.com>
2019-11-13 21:57:00 +0700

    fix: response TemplateData object

--- controller/response/mail.go
+++ controller/response/mail.go
@@ -2,10 +2,13 @@ package response
 
 // Mail : Parent Key -> Root
 type Mail struct {
-	To           string         `json:"to"`
-	From         string         `json:"from"`
-	Domain       string         `json:"domain"`
-	TemplateData string         `json:"templateData"`
-	Template     string         `json:"template"`
-	ReferenceID  string         `json:"referenceID"`
+	To           string `json:"to"`
+	From         string `json:"from"`
+	Domain       string `json:"domain"`
+	TemplateData struct {
+		Title string `json:"title"`
+		Body  string `json:"body"`
+	} `json:"templateData" validate:"required"`
+	Template    string `json:"template"`
+	ReferenceID string `json:"referenceID"`
 }
